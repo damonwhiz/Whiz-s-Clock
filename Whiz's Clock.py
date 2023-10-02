fd6c336162295625783c1bf116bc82016c0db8b0
damonwhiz <132417948+damonwhiz@users.noreply.github.com>
2023-10-02 10:26:25 -0500

    Add files via upload

---
+++ Whiz's Clock.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from time import strftime 
+
+myWindow = Tk()
+myWindow.title('My Clock')
+
+def time():
+    myTime = strftime('%H:%M:%S %p')
+    clock.config(text = myTime)
+    clock.after(1000, time)
+
+clock = Label(myWindow, font= ('arial', 40, 'bold'),
+                                background = 'purple',
+                                foreground = 'black')    
+clock.pack(anchor = 'center')
+time()
+
+mainloop()
